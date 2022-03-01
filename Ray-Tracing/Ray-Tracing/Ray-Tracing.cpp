c5748c2986104103346b57e73c1fa2078c7f75bc
Charlie <charlie3thomas.work@gmail.com>
2022-03-01 15:51:36 +0000

    Sphere render

--- Ray-Tracing/Ray-Tracing/Ray-Tracing.cpp
+++ Ray-Tracing/Ray-Tracing/Ray-Tracing.cpp
@@ -3,10 +3,12 @@
 #include <array>
 #include <fstream>
 #include "Vector3.h"
+#include "Ray.h"
 
-// Image
-static constexpr size_t IMAGE_WIDTH = 2560;
-static constexpr size_t IMAGE_HEIGHT = 1440;
+// Image.
+static constexpr float ASPECT_RATIO = 16.0 / 9.0;
+static constexpr size_t IMAGE_WIDTH = 1920;
+static constexpr size_t IMAGE_HEIGHT = IMAGE_WIDTH / ASPECT_RATIO/*1080*/;
 
 template <typename _T>
 static size_t unsigned_max() { return std::pow(2, 8 * sizeof(_T)) - 1; }
@@ -25,24 +27,63 @@ std::ostream& operator << (std::ostream& os, const image_t& image)
 	return os;
 }
 
-void foo(int a, int b, int c);
-
 static image_t image;
+
+bool hit(const unstd::Vector3<float>& centre, float radius, const unstd::Ray r)
+{
+	unstd::Vector3<float> oc = r.origin() - centre;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = (b * b) - (4 * a * c);
+	return (discriminant > 0);
+}
+
+unstd::Vector3<float> ray_colour(const unstd::Ray& r)
+{
+	if (hit(unstd::Vector3<float>(0.0, 0.0, -1.0), 0.5, r)) { return unstd::Vector3<float>(1.0, 0.0, 0.0); }
+
+	unstd::Vector3<> unit_dir = unit_vector(r.direction());
+	float t = 0.5f * (unit_dir.y() + 1.0f);
+	return (1.0f - t) * unstd::Vector3<float>(1.0, 1.0, 1.0) + // white
+					t * unstd::Vector3<float>(0.0, 0.0, 0.0); // black
+}
 
 int main()
 {
-	 //Render
-	 //For each pixel I need an RGB value
-	 //Each pixel will be an RGB triplet
+	// Camera.
+	float cam_height = 2.0;
+	float cam_width = ASPECT_RATIO * cam_height;
+	float focal_length = 1.0;
+
+	unstd::Vector3<float> origin = unstd::Vector3<float>(0, 0, 0);
+	unstd::Vector3<float> horizontal = unstd::Vector3<float>(cam_width, 0, 0);
+	unstd::Vector3<float> vertical = unstd::Vector3<float>(0, cam_height, 0);
+	unstd::Vector3<float> lower_left_corner = origin - horizontal / 2.0f - vertical / 2.0f - unstd::Vector3<float>(0, 0, focal_length);
+
+	 //Render.
+	 //For each pixel I need an RGB value.
+	 //Each pixel will be an RGB triplet.
 
 	for (size_t h = 0; h < IMAGE_HEIGHT; h++)
 	{
 		for (size_t w = 0; w < IMAGE_WIDTH; w++)
 		{
+			//pixel_t& pixel = image[IMAGE_WIDTH * h + w];
+			//pixel.e[0] = h * unsigned_max<colour_t>() / IMAGE_HEIGHT;
+			//pixel.e[1] = w * unsigned_max<colour_t>() / IMAGE_WIDTH;
+			//pixel.e[2] = 0.25 * unsigned_max<colour_t>();
+
+			float u = float(w) / (IMAGE_WIDTH - 1);
+			float v = float(h) / (IMAGE_HEIGHT - 1);
+
+			unstd::Ray r = unstd::Ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			unstd::Vector3<float> pixel_colour = ray_colour(r);
+
 			pixel_t& pixel = image[IMAGE_WIDTH * h + w];
-			pixel.e[0] = h * unsigned_max<colour_t>() / IMAGE_HEIGHT;
-			pixel.e[1] = w * unsigned_max<colour_t>() / IMAGE_WIDTH;
-			pixel.e[2] = 0.25 * unsigned_max<colour_t>();
+			pixel.e[0] = pixel_colour.x() * unsigned_max<colour_t>();
+			pixel.e[1] = pixel_colour.y() * unsigned_max<colour_t>();
+			pixel.e[2] = pixel_colour.z() * unsigned_max<colour_t>();
 		}
 	}
 
@@ -50,8 +91,4 @@ int main()
 	of << image;
 
 	return 0;
-}
-
-void foo(int a, int b, int c)
-{
 }
---
+++ Ray-Tracing/Ray-Tracing/Ray.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <stdlib.h>
+#include "Vector3.h"
+
+namespace unstd
+{
+	class Ray
+	{
+	public:
+		Ray() = default;
+		Ray(const unstd::Vector3<float>& origin,
+			const unstd::Vector3<float>& direction)
+			: r_orig(origin), r_dir(direction) {}
+		~Ray() = default;
+
+		unstd::Vector3<float> r_orig;
+		unstd::Vector3<float> r_dir;
+
+		unstd::Vector3<float> origin() const	{ return r_orig; }
+		unstd::Vector3<float> direction() const { return r_dir; }
+		unstd::Vector3<float> at(float t)		{ return r_orig + t * r_dir; }
+
+		float ray_time;
+	};
+}
